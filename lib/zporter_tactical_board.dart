cb8c3492a9f0762fef719d62d21d9cc54afcea41
sifat <sifatalamcep16@gmail.com>
2025-04-11 11:49:37 +0600

    fix library export

--- lib/zporter_tactical_board.dart
+++ lib/zporter_tactical_board.dart
@@ -5,6 +5,4 @@ library zporter_tactical_board;
 // Export the initialization function
 export 'app/services/injection_container.dart'
     show initializeTacticBoardDependencies;
-// Export the main widget you want to use
-export 'presentation/tactic/view/tacticboard_screen.dart'
-    show TacticboardScreen;
+export 'tactic_page.dart' show TacticPage;
