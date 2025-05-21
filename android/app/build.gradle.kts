36c63b89e57ef0b3ea855433bcc7c8b63b5ca9fd
sifat <sifatalamcep16@gmail.com>
2025-05-21 14:24:42 +0600

    updated to latest ndk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 android {
     namespace = "com.example.zporter_tactical_board"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -29,7 +29,7 @@ android {
         applicationId = "com.example.zporter_tactical_board"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
