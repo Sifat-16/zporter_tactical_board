14e98c512d734ee0dcc0a369405be2499238bd5a
Sifat <sifatalamcep16@gmail.com>
2025-07-17 10:46:37 +0600

    updated version & added build script

--- lib/app/config/version/version_info.dart
+++ lib/app/config/version/version_info.dart
@@ -1,6 +1,6 @@
 class AppInfo {
-  static const String version = "1.0.0.17";
-  static const String lastUpdated = "2025-06-21";
+  static const String version = "1.0.0.1";
+  static const String lastUpdated = "2025-07-17";
   static const String supportLinkName = "www.zporter.co/support";
   static const String supportLink = "https://zporter.co/support";
   static const String zporter_url = "https://onelink.to/zporter";
